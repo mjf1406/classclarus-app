6075b210ca32ab4438a3fc4b8cd46f3799de5ae4
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2025-04-09 19:21:14 +0900

    loading test

--- src/app/[classId]/loading.tsx
+++ src/app/[classId]/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="m-auto flex h-dvh w-dvw items-center justify-center">
+    <div className="m-auto flex h-full w-full items-center justify-center">
       <Loader2 className="h-24 w-24 animate-spin" />
     </div>
   );
