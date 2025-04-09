61d59255726867a940c4d591f5f81608c04a9005
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2025-04-09 20:37:38 +0900

    hrm

--- src/app/[classId]/loading.tsx
+++ src/app/[classId]/loading.tsx
@@ -1,5 +1,11 @@
-import Loader from "@/components/Loader";
+// import Loader from "@/components/Loader";
+import { Loader2 } from "lucide-react";
 
 export default function Loading() {
-  return <Loader />;
+  // return <Loader />;
+  return (
+    <div className="flex w-full items-center justify-center">
+      <Loader2 className="h-24 w-24 animate-spin" />
+    </div>
+  );
 }
