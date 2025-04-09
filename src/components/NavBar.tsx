e4d30529ef8c91232d2f323766728525ee6e7172
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2025-04-09 19:40:55 +0900

    more upgrades

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,34 @@
 /** @format */
+import React from "react";
+import { LogoHeader } from "./brand/logo";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { ThemeSelector } from "./theme-selector";
 
 export function Navbar() {
   return (
-    <nav className="bg-opacity-5 sticky top-0 z-50 h-16 w-full border-b px-2 backdrop-blur-xl backdrop-filter md:px-4"></nav>
+    <nav className="bg-opacity-5 bg-foreground/5 sticky top-0 z-10 w-full border-b backdrop-blur-xl backdrop-filter">
+      <div className="mx-auto flex h-16 max-w-5xl items-center justify-between px-2 md:px-4">
+        {/* Logo on the left */}
+        <LogoHeader />
+
+        {/* On the right, conditionally render Clerk user button if signed in, else sign in button */}
+        <div className="flex items-center justify-center gap-2">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <ThemeSelector />
+        </div>
+      </div>
+    </nav>
   );
 }
