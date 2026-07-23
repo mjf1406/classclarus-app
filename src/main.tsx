be5330508f872b2ffeef709e054a7ca1880094ca
Michael Fitzgerald <michael.fitzgerald.1406@gmail.com>
2026-07-23 17:37:12 +0900

    wire providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "./components/theme/theme-provider";
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 // Create a new router instance
 const router = createRouter({ routeTree });
-
+const queryClient = new QueryClient();
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
@@ -22,9 +26,13 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ConvexAuthProvider client={convex}>
+          <ThemeProvider>
+            <RouterProvider router={router} />
+          </ThemeProvider>
+        </ConvexAuthProvider>
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
